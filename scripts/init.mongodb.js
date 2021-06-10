e2104d3dae22b23cd6771e2223d1a9b9dedc4c15
Macsik121 <hiomimipad@gmail.com>
2021-06-10 13:44:42 +0300

    deploying the server to Heroku

---
+++ scripts/init.mongodb.js
@@ -0,0 +1,39 @@
+const goods = [
+    {
+        id: Math.floor(Math.random() * 1000000),
+        title: 'AMD Radeon RX 550 4gb',
+        cost: 183,
+        amount: 0,
+        inCart: false,
+    },
+    {
+        id: Math.floor(Math.random() * 1000000),
+        title: 'NVIDIA GeForce GTX 1650 4gb',
+        cost: 370,
+        amount: 0,
+        inCart: false,
+
+    },
+    {
+        id: Math.floor(Math.random() * 1000000),
+        title: 'NVIDIA GeForce GTX 1060 6gb',
+        cost: 500,
+        amount: 0,
+        inCart: false,
+    },
+    {
+        id: Math.floor(Math.random() * 1000000),
+        title: 'AMD Radeon 6700 xt 12gb',
+        cost: 1799,
+        amount: 0,
+        inCart: false,
+    }
+];
+
+db.goods.drop();
+db.goods.insertMany(goods);
+db.goodsInCart.drop();
+db.goodsInCart.insertOne({init: ''});
+db.goodsInCart.deleteOne({init: ''});
+db.amountGoodsInCart.drop();
+db.amountGoodsInCart.insertOne({_id: 'amountOfGoodsInCart', current: 0});
